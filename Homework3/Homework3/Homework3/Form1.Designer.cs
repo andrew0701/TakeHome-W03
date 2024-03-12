c0debd191acb43551facf26440f69dc5cd45d1f5
andrew0701 <161418900+andrew0701@users.noreply.github.com>
2024-03-12 23:02:49 +0700

    Andrew Hendrawan-0706022310003-TakeHome-W03
    takehome w03

---
+++ Homework3/Homework3/Homework3/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Homework3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxusername = new System.Windows.Forms.TextBox();
+            this.textBoxpassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonlogin = new System.Windows.Forms.Button();
+            this.buttonregister = new System.Windows.Forms.Button();
+            this.buttondeposit = new System.Windows.Forms.Button();
+            this.buttonwithdraw = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonlogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxusername
+            // 
+            this.textBoxusername.Location = new System.Drawing.Point(128, 129);
+            this.textBoxusername.Name = "textBoxusername";
+            this.textBoxusername.Size = new System.Drawing.Size(160, 19);
+            this.textBoxusername.TabIndex = 0;
+            // 
+            // textBoxpassword
+            // 
+            this.textBoxpassword.Location = new System.Drawing.Point(128, 164);
+            this.textBoxpassword.Name = "textBoxpassword";
+            this.textBoxpassword.Size = new System.Drawing.Size(160, 19);
+            this.textBoxpassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "UC BANK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // buttonlogin
+            // 
+            this.buttonlogin.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonlogin.Location = new System.Drawing.Point(30, 247);
+            this.buttonlogin.Name = "buttonlogin";
+            this.buttonlogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonlogin.TabIndex = 5;
+            this.buttonlogin.Text = "login";
+            this.buttonlogin.UseVisualStyleBackColor = true;
+            this.buttonlogin.Click += new System.EventHandler(this.buttonlogin_Click);
+            // 
+            // buttonregister
+            // 
+            this.buttonregister.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonregister.Location = new System.Drawing.Point(213, 247);
+            this.buttonregister.Name = "buttonregister";
+            this.buttonregister.Size = new System.Drawing.Size(75, 23);
+            this.buttonregister.TabIndex = 6;
+            this.buttonregister.Text = "register";
+            this.buttonregister.UseVisualStyleBackColor = true;
+            this.buttonregister.Click += new System.EventHandler(this.buttonregister_Click);
+            // 
+            // buttondeposit
+            // 
+            this.buttondeposit.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttondeposit.Location = new System.Drawing.Point(30, 184);
+            this.buttondeposit.Name = "buttondeposit";
+            this.buttondeposit.Size = new System.Drawing.Size(75, 23);
+            this.buttondeposit.TabIndex = 7;
+            this.buttondeposit.Text = "Deposit";
+            this.buttondeposit.UseVisualStyleBackColor = true;
+            this.buttondeposit.Visible = false;
+            this.buttondeposit.Click += new System.EventHandler(this.buttondeposit_Click);
+            // 
+            // buttonwithdraw
+            // 
+            this.buttonwithdraw.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonwithdraw.Location = new System.Drawing.Point(30, 213);
+            this.buttonwithdraw.Name = "buttonwithdraw";
+            this.buttonwithdraw.Size = new System.Drawing.Size(75, 23);
+            this.buttonwithdraw.TabIndex = 8;
+            this.buttonwithdraw.Text = "Withdraw";
+            this.buttonwithdraw.UseVisualStyleBackColor = true;
+            this.buttonwithdraw.Visible = false;
+            this.buttonwithdraw.Click += new System.EventHandler(this.buttonwithdraw_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Username";
+            this.label4.Visible = false;
+            // 
+            // buttonlogout
+            // 
+            this.buttonlogout.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonlogout.Location = new System.Drawing.Point(213, 74);
+            this.buttonlogout.Name = "buttonlogout";
+            this.buttonlogout.Size = new System.Drawing.Size(75, 23);
+            this.buttonlogout.TabIndex = 10;
+            this.buttonlogout.Text = "logout";
+            this.buttonlogout.UseVisualStyleBackColor = true;
+            this.buttonlogout.Visible = false;
+            this.buttonlogout.Click += new System.EventHandler(this.buttonlogout_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 352);
+            this.Controls.Add(this.buttonlogout);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonwithdraw);
+            this.Controls.Add(this.buttondeposit);
+            this.Controls.Add(this.buttonregister);
+            this.Controls.Add(this.buttonlogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxpassword);
+            this.Controls.Add(this.textBoxusername);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxusername;
+        private System.Windows.Forms.TextBox textBoxpassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonlogin;
+        private System.Windows.Forms.Button buttonregister;
+        private System.Windows.Forms.Button buttondeposit;
+        private System.Windows.Forms.Button buttonwithdraw;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonlogout;
+    }
+}
+
---
+++ Homework3/Homework3/Homework3/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework3
+{
+    public partial class Form1 : Form
+    {
+        List<string> username = new List<string>();
+        List<string> password = new List<string>();
+        List<int> money = new List<int>();
+        int cek = 0;
+        int user = 0;
+        int cekdepo = 0;
+        int cekwid = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonregister_Click(object sender, EventArgs e)
+        {
+            if (cek == 0)
+            {
+                buttonlogin.Visible = false;
+                MessageBox.Show("Please enter new registration");
+                cek = 1;
+            }
+            else
+            {
+                buttonlogin.Visible = true;
+                cekuserdanpass();
+                cek = 0;
+            }
+
+
+        }
+        private void cekuserdanpass()
+        {
+            int cekregis = 0;
+            foreach (string username in username)
+            {
+                if (username == textBoxusername.Text)
+                {
+                    MessageBox.Show("User already registered");
+                    cekregis = 1;
+                    break;
+                }
+            }
+            if (cekregis == 0)
+            {
+                MessageBox.Show("Succesfully registered");
+                username.Add(textBoxusername.Text);
+                password.Add(textBoxpassword.Text);
+                money.Add(0);
+                textBoxusername.Text = "";
+                textBoxpassword.Text = "";
+            }
+        }
+
+        private void buttonlogin_Click(object sender, EventArgs e)
+        {
+            int ceklogin = 0;
+            user = -1;
+            foreach (string username in username)
+            {
+                user++;
+                if (username == textBoxusername.Text)
+                {
+                    ceklogin = 1;
+                    break;
+                }
+            }
+            if (ceklogin == 1 && password[user] == textBoxpassword.Text)
+            {
+                MessageBox.Show("Welcome");
+                label4.Visible = true;
+                buttondeposit.Visible = true;
+                buttonwithdraw.Visible = true;
+                buttonlogout.Visible = true;
+                buttonregister.Visible = false;
+                buttonlogin.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                textBoxpassword.Visible = false;
+                textBoxusername.Visible = false;
+                label4.Text = "Saldo: " + money[user].ToString("C", new CultureInfo("id-ID"));
+            }
+            else
+            {
+                MessageBox.Show("No Username Found");
+            }
+        }
+
+        private void buttonlogout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Succesfully logged out");
+            label4.Visible = false;
+            buttondeposit.Visible = false;
+            buttonwithdraw.Visible = false;
+            buttonlogout.Visible = false;
+            buttonregister.Visible = true;
+            buttonlogin.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            textBoxpassword.Visible = true;
+            textBoxusername.Visible = true;
+        }
+
+        private void buttondeposit_Click(object sender, EventArgs e)
+        {
+            if (cekdepo == 0)
+            {
+                label2.Text = "Amount:";
+                label2.Visible = true;
+                textBoxusername.Text = "";
+                textBoxusername.Visible = true;
+                buttonwithdraw.Visible = false;
+                cekdepo = 1;
+            }
+            else
+            {
+                int moneydeposit = Convert.ToInt32(textBoxusername.Text);
+                int moneynow = money[user];
+
+                if (moneydeposit <= 0)
+                {
+                    MessageBox.Show("cannot deposit less than 1");
+                }
+                else
+                {
+                    MessageBox.Show("Successfully Deposited " + moneydeposit.ToString());
+                    money[user] = moneynow + moneydeposit;
+                }
+                label4.Text = "Saldo: " + money[user].ToString("C", new CultureInfo("id-ID"));
+                label2.Text = "Username";
+                label2.Visible = false;
+                textBoxusername.Visible = false;
+                buttonwithdraw.Visible = true;
+                textBoxusername.Text = "";
+                cekdepo = 0;
+            }
+        }
+
+        private void buttonwithdraw_Click(object sender, EventArgs e)
+        {
+            if (cekwid == 0)
+            {
+                label2.Text = "Amount:";
+                label2.Visible = true;
+                textBoxusername.Visible = true;
+                textBoxusername.Text = "";
+                buttondeposit.Visible = false;
+                cekwid = 1;
+            }
+            else
+            {
+                int moneywithdraw = Convert.ToInt32(textBoxusername.Text);
+                int moneynow = money[user];
+                if (moneynow < moneywithdraw)
+                {
+                    MessageBox.Show("Balance too small");
+                }
+                else if (moneywithdraw <= 0)
+                {
+                    MessageBox.Show("cannot withdraw less than 1");
+                }
+                else
+                {
+                    MessageBox.Show("Successfully Withdrawn " + moneywithdraw.ToString());
+                    money[user] = moneynow - moneywithdraw;
+                }
+                label4.Text = "Saldo: " + money[user].ToString("C", new CultureInfo("id-ID"));
+                label2.Text = "Username";
+                label2.Visible = false;
+                textBoxusername.Visible = false;
+                buttondeposit.Visible = true;
+                textBoxusername.Text = "";
+                cekwid = 0;
+            }
+        }
+    }
+}
